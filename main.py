db48df9f13dd90ae58780e754ab86a15e1181573
SN-Grotesque <z7z-h@outlook.com>
2022-09-20 05:47:12 +0800

    eHentai

--- main.py
+++ main.py
@@ -2,17 +2,9 @@ from eht.utils import ProxyInfo
 import json
 import eht
 
-with open('000_ArtworkList.json', 'r', encoding='utf-8') as f:
-    Artworks = json.loads(f.read())
+url = 'https://e-hentai.org/g/123456abc/123456abc/'
+directoryName = '123'
+r18g = 1
 
-for x in Artworks:
-    url = Artworks[x]['artworkUrl']
-    dn  = Artworks[x]['artworkName']
-    print(url, dn)
-
-# url = 'https://e-hentai.org/g/123456abc/123456abc/'
-# directoryName = '123'
-# r18g = 1
-
-# e = eht.ehentai(url, directoryName, r18g = r18g, proxyinfo = ProxyInfo)
-# e.download
+e = eht.ehentai(url, directoryName, r18g = r18g, proxyinfo = ProxyInfo)
+e.download
